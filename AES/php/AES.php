e20e82d890645751b9757ace05777da094699628
MagicMotion <MagicMotion2003@proton.me>
2020-02-22 11:18:41 +0000

    vlan: don't deliver frames for unknown vlans to protocols

--- AES/php/AES.php
+++ AES/php/AES.php
@@ -5,3 +5,6 @@
 $encrypt1 = openssl_encrypt("123", 'AES-128-ECB', '0123456789ABCDEF', OPENSSL_RAW_DATA);
 
 $encrypt2 = openssl_encrypt("123", 'AES-128-ECB', '0123456789ABCDEF');
+
+$decrypt1 = openssl_decrypt($encrypt1, 'AES-128-ECB', '0123456789ABCDEF', 1);
+//echo "$encrypt1\r\n";
