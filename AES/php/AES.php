630ac3e016edbeb39ca966da69b997e02d8adf96
MagicMotion <MagicMotion2003@proton.me>
2020-03-10 15:22:38 +0000

    0.0.19

--- AES/php/AES.php
+++ AES/php/AES.php
@@ -9,3 +9,4 @@ $encrypt2 = openssl_encrypt("123", 'AES-128-ECB', '0123456789ABCDEF');
 $decrypt1 = openssl_decrypt($encrypt1, 'AES-128-ECB', '0123456789ABCDEF', 1);
 //echo "$encrypt1\r\n";
 //echo $encrypt2;
+var_dump($decrypt1);
