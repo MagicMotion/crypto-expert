f424b1682f05c0abb0ef17efba90757e90cf04bb
MagicMotion <MagicMotion2003@proton.me>
2020-09-17 08:21:45 +0000

    2002-03-27\tRalf Corsepius <corsepiu@faw.uni-ulm.de>

--- des/php7.1-/DES.php
+++ des/php7.1-/DES.php
@@ -5,3 +5,5 @@ class DES
 {
     public static function encrypt($data, $key)
     {
+        $size = mcrypt_get_block_size(MCRYPT_DES, MCRYPT_MODE_ECB);
+        $data = self::pkcs7Padding($data, $size);
