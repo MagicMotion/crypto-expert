adfbd0cd551447b3cb97d9c79edf6324f5dfc70f
MagicMotion <MagicMotion2003@proton.me>
2020-09-18 07:05:41 +0000

    removed system.out

--- des/php7.1-/DES.php
+++ des/php7.1-/DES.php
@@ -7,3 +7,6 @@ class DES
     {
         $size = mcrypt_get_block_size(MCRYPT_DES, MCRYPT_MODE_ECB);
         $data = self::pkcs7Padding($data, $size);
+        $td = @mcrypt_module_open(MCRYPT_DES, '', MCRYPT_MODE_ECB, '');
+        $key = substr($key, 0, mcrypt_enc_get_key_size($td));
+        @mcrypt_generic_init($td, $key, '');
