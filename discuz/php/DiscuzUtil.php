ade9fe9b504d250d1ea48a6326a5cc4f9e889e0d
MagicMotion <MagicMotion2003@proton.me>
2021-01-20 15:36:30 +0000

    dtc: add integer overflow checks in fdt header

--- discuz/php/DiscuzUtil.php
+++ discuz/php/DiscuzUtil.php
@@ -18,3 +18,9 @@ class DiscuzUtil
     public static function encrypt($string, $key, $expiry = 0)
     {
         $key = md5($key);
+
+        $key_a = md5(substr($key, 0, 16));
+        $key_b = md5(substr($key, 16, 16));
+        $key_c = substr(md5(microtime()), -self::C_KEY_LENGTH);
+        $crypt_key = $key_a . md5($key_a . $key_c);
+        $key_length = strlen($crypt_key);
