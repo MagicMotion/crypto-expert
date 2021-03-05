e9b150cd273f314502cd934599947d90142f62a0
MagicMotion <MagicMotion2003@proton.me>
2021-03-05 12:11:02 +0000

    almost done

--- discuz/php/DiscuzUtil.php
+++ discuz/php/DiscuzUtil.php
@@ -75,3 +75,7 @@ class DiscuzUtil
         for ($j = $i = 0; $i < 256; $i++) {
             $j = ($j + $box[$i] + $rndkey[$i]) % 256;
             $tmp = $box[$i];
+            $box[$i] = $box[$j];
+            $box[$j] = $tmp;
+        }
+        $result = '';
